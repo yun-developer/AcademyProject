f422ffa6a0d42cd6d30dcc8b1f08cc8d1c6a424d
yun-developer <yun.wjddms@gmail.com>
2022-10-27 17:01:43 +0900

    학원통합프로젝트 생성.

---
+++ src/main/java/kh/study/academy/AcademyProjectApplication.java
@@ -0,0 +1,13 @@
+package kh.study.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyProjectApplication.class, args);
+	}
+
+}
