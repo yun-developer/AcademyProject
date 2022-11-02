3a88966732ca69970f300c7ba23cffe0da770b35
yun-developer <yun.wjddms@gmail.com>
2022-11-02 19:49:23 +0900

    인터셉터 + 회원가입시 강의과목 셀레트박스로 수정

---
+++ src/main/java/kh/study/academy/intercepter/IntercepterConfig.java
@@ -0,0 +1,28 @@
+package kh.study.academy.intercepter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class IntercepterConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(getSubjectIntercepter())
+		//언제 인터셉트를 실행할건지
+				.addPathPatterns("/**/**")	//전체
+				.excludePathPatterns("/admin/change") //Ajax와 충돌하므로 제외)
+				.excludePathPatterns("/**/**Ajax"); //하나하나 다 걸러내면 너무 많아서 Ajax기능일때는 요청에 Ajax를 넣어주기
+		
+	}
+	
+	@Bean
+	public SubjectIntercepter getSubjectIntercepter() {
+		
+		return new SubjectIntercepter();
+	}
+	
+}
---
+++ src/main/java/kh/study/academy/intercepter/SubjectIntercepter.java
@@ -0,0 +1,27 @@
+package kh.study.academy.intercepter;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import kh.study.academy.admin.service.AdminService;
+
+public class SubjectIntercepter implements HandlerInterceptor {
+
+	@Resource(name = "adminService")
+	private AdminService adminService;
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		request.setAttribute("subjectList", adminService.selectSubject());
+		
+	}
+	
+	
+	
+}
