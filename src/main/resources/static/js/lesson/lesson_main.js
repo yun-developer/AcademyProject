35eba04c8d9d78f05ba01917e83ec4f1b1f189bc
yun-developer <yun.wjddms@gmail.com>
2022-11-07 16:37:52 +0900

    메인페이지 차트템플릿 위치 및 구조 수정

--- src/main/resources/static/js/lesson/lesson_main.js
+++ src/main/resources/static/js/lesson/lesson_main.js
@@ -33,3 +33,20 @@ const myChart = new Chart(ctx, {
         }
     }
 });
+
+
+/*const cht1 = document.getElementById('mySecondChart').getContext('2d');
+const mySecondChart = {
+  labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
+  datasets: [
+    {
+      label: 'Dataset',
+      data: Utils.numbers({count: 6, min: -100, max: 100}),
+      borderColor: Utils.CHART_COLORS.red,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+      pointStyle: 'circle',
+      pointRadius: 10,
+      pointHoverRadius: 15
+    }
+  ]
+};*/
