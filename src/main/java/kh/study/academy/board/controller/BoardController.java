25532505a4037d19bca1f50c708e54772d06f3f8
kwseon <javaakws@gmail.com>
2022-11-10 13:58:31 +0900

    BoardNotice 게시판 디자인 수정

--- src/main/java/kh/study/academy/board/controller/BoardController.java
+++ src/main/java/kh/study/academy/board/controller/BoardController.java
@@ -65,6 +65,16 @@ public class BoardController {
 		return "redirect:/board/noticeList";
 	}
 	
+	//공지사항 상세 읽기
+	@GetMapping("/noticeDetail")
+	public String noticeDetail() {
+		
+		return "content/board/notice_detail";
+	}
+	
+	
+	
+	
 	
 	//자유게시판 리스트
 	@GetMapping("/freeList")
@@ -80,6 +90,40 @@ public class BoardController {
 		return "content/board/write_free_page";
 	}
 		
+	//자유게시판 글쓰기
+	@PostMapping("/writeFree")
+	public String writeFree2(BoardVO boardVO, Authentication authentication, List<MultipartFile> imgs) {
+		User user = (User)authentication.getPrincipal();
+		boardVO.setTeacherId(user.getUsername());
+		
+		int nextBoardNum = boardService.getNextBoardNum();
+		boardVO.setBoardNum(nextBoardNum);
+		
+		//다중 이미지 파일 첨부
+		List<BoardImgVO> uploadList = UploadFileUtil2.multiUploadFile(imgs);
+		
+		//이미지 정보를 insert 하기 위한 데이터를 가진 uploadList에 조회한 boardNum 값도 넣어줌
+		for(BoardImgVO vo : uploadList) {
+			vo.setBoardNum(nextBoardNum);
+		}
+		
+		//boardVO가 첨부파일에 대한 정보를 세터로 다 가지게 됨
+		boardVO.setImgList(uploadList);
+		
+		boardService.insertFree(boardVO);
+		
+		return "redirect:/board/freeList";
+	}
+	
+	
+	//자유게시판 상세 읽기
+	@GetMapping("/freeDetail")
+	public String freeDetail() {
+		
+		return "content/board/free_detail";
+	}
+		
+	
 	
 	
 }
--- src/main/resources/static/js/board/board_free_list.js
+++ src/main/resources/static/js/board/board_free_list.js
@@ -1,3 +1,86 @@
+//제목줄 체크박스
+const checkAll = document.querySelector('#checkAll');
+
+//제목 줄 제외한 모든 체크박스
+const chks = document.querySelectorAll('.chk');
+
+//체크박스 전체선택, 전체해제 이벤트
+checkAll.addEventListener('click', function(){
+	//제목줄에서 체크박스의 체크여부
+	const isChecked = checkAll.checked; // true, false
+	
+	//모든 체크박스	
+	const checkBoxes = document.querySelectorAll('.chk');	
+	
+	//제목줄 체크박스가 체크되었다면
+	if(isChecked) {
+		//모든 체크박스를 체크함. 클래스가 chk인 것 모두 가져옴
+		
+		for(const checkBox of checkBoxes) {
+			checkBox.checked = true;
+		}
+	}		
+	else {
+		for(const checkBox of checkBoxes) {
+			checkBox.checked = false;
+		}
+	}
+});
+
+
+
+//리스트 체크박스 선택 시
+for(const chk of chks){
+	chk.addEventListener('click', chk=>{ //밑줄 와이...?
+		//아래에 있는 전체 체크박스의 수 
+		const cnt = chks.length;
+		//아래에 있는 전체 체크박스 중 체크된 수
+		const checkedCnt = document.querySelectorAll('.chk:checked').length;
+		
+		//수가 같으면 제목 체크박스도 체크
+		if(cnt ==checkedCnt ){
+			document.querySelector('#checkAll').checked = true;
+		}
+		//수가 다르면 제목 체크박스도 해체
+		else{
+			document.querySelector('#checkAll').checked = false;
+		}
+		
+	});
+}
+
+
+
+
+
+//삭제 버튼 클릭
+function goDelete() {
+	const deleteForm = document.querySelector('#deleteStuForm');	
+	
+	//체크한 cartCode 다 들고 온다.
+	const checkedChks = document.querySelectorAll('.chk:checked');
+	
+	if(checkedChks.length == 0) {
+		alert('삭제할 학생을 선택하세요');
+		return ;
+	}
+	
+	//문자열로 만들어서 던져 줌
+	let studentCodes = '';
+	for(const checkedChk of checkedChks) {
+		studentCodes = studentCodes + checkedChk.value + ',';  
+	}
+	
+	deleteForm.querySelector('input').value = studentCodes;  // 넘어오는 name과 컨트롤러의 매개변수 이름 같으면 알아서 받아줌
+	
+	deleteForm.submit();
+}
+
+
+
+
+
+
 //페이징 라이브러리
 var box = paginator({
     table: document.getElementById("table_box_bootstrap").getElementsByTagName("table")[0],
