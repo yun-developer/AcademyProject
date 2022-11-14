5aab419070fb5fcb0e3b6ec94d505c3dd409377c
kwseon <javaakws@gmail.com>
2022-11-14 09:45:28 +0900

    공지사항, 자유게시판 댓글 삭제

--- src/main/java/kh/study/academy/reply/controller/ReplyController.java
+++ src/main/java/kh/study/academy/reply/controller/ReplyController.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,6 +35,15 @@ public class ReplyController {
 		return "redirect:/board/noticeDetail";
 	}
 	
+
+	//공지사항 댓글 삭제
+	@GetMapping("/noticeDelete")
+	public String replyNoticeDelete(String replyNum, int boardNum, RedirectAttributes redirect) {
+		replyService.deleteReply(replyNum);
+		
+		redirect.addAttribute("boardNum", boardNum);
+		return "redirect:/board/noticeDetail";
+	}
 	
 	//자유게시판 댓글 작성
 	@RequestMapping("/writeFreeReply")
@@ -50,7 +60,14 @@ public class ReplyController {
 		return "redirect:/board/freeDetail";
 	}
 	
-	
-	
+
+	//자유게시판 댓글 삭제
+	@GetMapping("/freeDelete")
+	public String replyFreeDelete(String replyNum, int boardNum, RedirectAttributes redirect) {
+		replyService.deleteReply(replyNum);
+		
+		redirect.addAttribute("boardNum", boardNum);
+		return "redirect:/board/freeDetail";
+	}
 	
 }
