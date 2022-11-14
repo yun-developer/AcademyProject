c3e81b9d08cd5516b323e3cb8ec7adfb84136ee7
kwseon <javaakws@gmail.com>
2022-11-14 17:20:54 +0900

    좋아요 하트 삭제

--- src/main/resources/static/js/board/notice_detail.js
+++ src/main/resources/static/js/board/notice_detail.js
@@ -6,16 +6,15 @@ function likeAjax(boardNum) {
 		data: {'boardNum':boardNum}, //필요한 데이터를 컨트롤러로 전달
 		success: function(result) { //컨트롤러에서 리턴된 데이터 result로 받음
 			alert(111111111111)
-			const extractTextPattern = /(<([^>]+)>)/gi;
-			let likeButton = document.querySelector('.likeButton');
+			let likeDiv = document.querySelector('#likeDiv');
 					
 					
-			 let src = likeButton.innerHTML;
-			  
-			 let extractedText = src.replace(extractTextPattern, '');
-						
-			const str = '';
+			likeDiv.innerHTML = '';
 			
+			
+			let str = '';
+			
+						
 		},
 		error: function() {
 			alert('실패');
