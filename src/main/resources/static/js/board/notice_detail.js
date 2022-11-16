f6b0ad25bfb23258eaeea193b3fd7c4bbdea0b7d
kwseon <javaakws@gmail.com>
2022-11-16 14:21:51 +0900

    댓글 글자수 세기 완료

--- src/main/resources/static/js/board/notice_detail.js
+++ src/main/resources/static/js/board/notice_detail.js
@@ -34,4 +34,19 @@ function likeAjax(boardNum) {
 }
 
 
-
+$("#replyLength").keyup(function(e) {
+	    //console.log("키업!");
+		var content = $(this).val();
+		$("#replyLengthCheck").text("(" + content.length + " / 최대 500자)"); //실시간 글자수 카운팅
+		
+		if (content.length > 500) {
+			
+			Swal.fire({
+			  icon: 'warning',  // 여기다가 아이콘 종류를 쓰면 됩니다.                     
+			  title: '최대 500자까지 입력 가능합니다.',    
+			});
+			
+			$(this).val(content.substring(0, 500));
+			$('#replyLengthCheck').text("(500 / 최대 500자)");
+		}
+	});
