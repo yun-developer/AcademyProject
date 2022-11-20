f7cda9e757ca061fa4266192cd4edb1e31f3b8e1
bbang <bbang@DESKTOP-G1EU3N1>
2022-11-20 16:55:58 +0900

    평가등록 모달로 해당 학생의 데이터 전달 및 평가등록 기능 구현 완성

--- src/main/resources/static/js/test/testManage.js
+++ src/main/resources/static/js/test/testManage.js
@@ -19,21 +19,29 @@ const searchYear = document.querySelector('#searchYear');
 
 //평가등록 모달을 눌렀을 때 실행되는 함수
 
-function openRegTestModal(code){
-	//학생코드
-	//const studentCode = document.querySelectorAll('.stuCode');
+function openRegTestModal(code, name){
+	
+	//학생코드 모달 hidden input value에 넣기
+	document.querySelector('#stuCodeForReg').value = code;
+	const stuentCode = document.querySelector('#stuCodeForReg').value;
+	
+	alert(stuentCode);
+	
+	//학생 이름 모달 input value에 넣기
+	document.querySelector('#stuNameForReg').value = name;
+	const stuentName = document.querySelector('#stuNameForReg').value;
 	
 	
+	alert(stuentName);
 	
-	//학생 이름 
-	const studentName = document.querySelectorAll('.stuName')[0].innerText;
-	const lessonName = document.querySelector('.stuLesson').innerText;
 	
-	alert(studentName);
-	alert(lessonName);
+	//const lessonName = document.querySelector('.stuLesson').innerText;
+	
+	//alert(studentName);
+	//alert(lessonName);
 	
 	//학생이름을 모달창에 보여주게 하기
-	document.querySelector('#stuCodeForReg').value = studentName;
+	//document.querySelector('#stuCodeForReg').value = studentName;
 }
 
 
@@ -55,18 +63,32 @@ function openChangeTestModal(text){
 
 
 
-//아직 하는중
 //평가수정 모달 내 저장 버튼을 눌렀을 때 실행되는 함수
 function regTest()  {
 	
-	//학생 코드 
-	const studentCode = document.querySelector('.stuCode').value;
+	 Swal.fire({
+	   title: '평가 등록 완료',
+	   text: '해당 학생의 평가 등록이 완료되었습니다',
+	   icon: 'success',
+	   
+	   showCancelButton: false, // cancel버튼 보이기. 기본은 원래 없음
+	   confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+	   cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+	   confirmButtonText: '확인', // confirm 버튼 텍스트 지정
+	   
+	   
+	}).then(result => {
+	   // 만약 Promise리턴을 받으면,
+	   if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
+	   
+	     document.querySelector('#regTestForm').submit();
+	   }
+	});
+		
 	
-	alert(studentCode);
+	//document.querySelector('#regTestForm').submit();
+
 	
-	document.querySelector('#stuCodeForReg').value = studentCode;
-	
-	document.querySelector('#regTestForm').submit();
 }
 
 
