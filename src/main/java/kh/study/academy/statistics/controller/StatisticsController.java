ed364f2efe5c19b0547c807f5b48b03da820a28e
bb9oo <hyeonjubb@gmail.com>
2022-11-21 21:36:52 +0900

    ajax실행 후 map으로 넘어온 과목별 학생 수 데이터를 js에서 차트에 맞게 값 뽑아내기

--- src/main/java/kh/study/academy/statistics/controller/StatisticsController.java
+++ src/main/java/kh/study/academy/statistics/controller/StatisticsController.java
@@ -1,9 +1,12 @@
 package kh.study.academy.statistics.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import kh.study.academy.admin.service.AdminService;
+import kh.study.academy.admin.vo.SubjectVO;
 import kh.study.academy.statistics.service.StatisticsService;
 import kh.study.academy.statistics.vo.StudentCntPerGrade;
 import kh.study.academy.statistics.vo.StudentCntPerSubject;
@@ -23,7 +28,8 @@ public class StatisticsController {
 	@Resource(name = "statisticsService")
 	private StatisticsService statisticsService;
 	
-	
+	@Resource(name = "adminService")
+	private AdminService adminService;
 	
 	//학생현황 페이지로 이동 
 	@GetMapping("/studentStatus")
@@ -32,47 +38,48 @@ public class StatisticsController {
 		return "content/statistics/studentStatus";
 	}
 	
-	//차트를 그릴 테이터를 조회하는 메소드
-	@ResponseBody
-	@PostMapping("/studentStatusAjax")
-	public List<StudentCntPerGrade> studentStatusAjax(Model model) {
-		//학년별 학생 수 조회
-		List<StudentCntPerGrade> studentCntPerGrade = statisticsService.selectNumByStuYear();
-		
-		
-		//과목별 학생 수 조회 
-		//List<StudentCntPerSubject> StudentCntPerSubject = statisticsService.selectNumByStuSubject();
 	
-		
-		
-		return studentCntPerGrade;
-	}
 	
-	/*
-	 * //차트를 그릴 테이터를 조회하는 메소드
-	 * 
-	 * @ResponseBody
-	 * 
-	 * @PostMapping("/studentStatusAjax") public Map<String, Object>
-	 * studentStatusAjax(Model model) {
-	 * 
-	 * //0 Map<String, Object> paramMap = new HashMap<>();
-	 * 
-	 * paramMap.put("studentCntPerGrade", statisticsService.selectNumByStuYear());
-	 * paramMap.put("studentCntPerSubject",
-	 * statisticsService.selectNumByStuSubject());
-	 * 
-	 * //학년별 학생 수 조회 List<StudentCntPerGrade> studentCntPerGrade =
-	 * statisticsService.selectNumByStuYear();
-	 * 
-	 * 
-	 * //과목별 학생 수 조회 List<StudentCntPerSubject> studentCntPerSubject =
-	 * statisticsService.selectNumByStuSubject();
-	 * 
-	 * 
-	 * 
-	 * return paramMap; }
-	 */
+	
+	  //차트를 그릴 테이터를 조회하는 메소드
+	  @ResponseBody
+	  @PostMapping("/studentStatusAjax") public Map<String, Object>
+	  studentStatusAjax(Model model) {
+	  
+		  Map<String, Object> paramMap = new HashMap<>();
+		  
+		  //map 학년별 학생 수 
+		  paramMap.put("studentCntPerGrade", statisticsService.selectNumByStuYear());
+		 
+		  
+		  /////////////////////////////////////////////////////
+		  
+		  Map<String, List<Integer>> chart2_data = new HashMap<>();
+		  
+		  
+		  //전체 과목 조회
+		  List<SubjectVO> subjectList = adminService.selectSubject();
+		  
+		  for(SubjectVO subject : subjectList) {
+			  //과목명 조회
+			  String subjectCode = subject.getSubjectCode();
+			  
+			  //과목당 1,2,3 학년의 학생 수 조회
+			  List<Integer> stuCntList = statisticsService.selectNumByStuSubject(subjectCode);
+			  
+			  chart2_data.put(subject.getSubjectName(), stuCntList);
+			  
+		  }
+		  
+		  
+		  //map 과목별 학생 수
+		  paramMap.put("studentCntPerSubject", chart2_data);
+		  
+		  
+		  
+		  return paramMap; 
+	  }
+	 
 	
 	
 	//평가관리 분석 페이지로 이동 
--- src/main/resources/static/js/statistics/studentStatus.js
+++ src/main/resources/static/js/statistics/studentStatus.js
@@ -1,182 +1,14 @@
+// 1. 그릴 차트를 선택한다
+// 2. 차트 데이터 설정을 보고 맞춤형 쿼리를 작성한다.(*****)
+// 3. 페이지 들어오자마자 차트 그리는 함수 실행
+// 4. 차트 그리는 함수 안에 에이작스 실행
+// 5. 에이작스에서 컨트롤러 이동
+// 6. 컨트롤러에서 차트를 그릴 데이터를 조회하는 메소드를 작성하고 다시 map으로 데이터 보내기
+// 7. map으로 받아온 데이터를 잘 뽑아내기 (key,value)(***)
 
 drawChart();
 
-//차트 1
-      
-      
-       /* var options = {
-          series: [{
-          name: 'Income',
-          type: 'column',
-          data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
-        }, {
-          name: 'Cashflow',
-          type: 'column',
-          data: [1.1, 3, 3.1, 4, 4.1, 4.9, 6.5, 8.5]
-        }, {
-          name: 'Revenue',
-          type: 'line',
-          data: [20, 29, 37, 36, 44, 45, 50, 58]
-        }],
-          chart: {
-          height: 350,
-          type: 'line',
-          stacked: false
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          width: [1, 1, 4]
-        },
-        title: {
-          text: 'XYZ - Stock Analysis (2009 - 2016)',
-          align: 'left',
-          offsetX: 110
-        },
-        xaxis: {
-          categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
-        },
-        yaxis: [
-          {
-            axisTicks: {
-              show: true,
-            },
-            axisBorder: {
-              show: true,
-              color: '#008FFB'
-            },
-            labels: {
-              style: {
-                colors: '#008FFB',
-              }
-            },
-            title: {
-              text: "Income (thousand crores)",
-              style: {
-                color: '#008FFB',
-              }
-            },
-            tooltip: {
-              enabled: true
-            }
-          },
-          {
-            seriesName: 'Income',
-            opposite: true,
-            axisTicks: {
-              show: true,
-            },
-            axisBorder: {
-              show: true,
-              color: '#00E396'
-            },
-            labels: {
-              style: {
-                colors: '#00E396',
-              }
-            },
-            title: {
-              text: "Operating Cashflow (thousand crores)",
-              style: {
-                color: '#00E396',
-              }
-            },
-          },
-          {
-            seriesName: 'Revenue',
-            opposite: true,
-            axisTicks: {
-              show: true,
-            },
-            axisBorder: {
-              show: true,
-              color: '#FEB019'
-            },
-            labels: {
-              style: {
-                colors: '#FEB019',
-              },
-            },
-            title: {
-              text: "Revenue (thousand crores)",
-              style: {
-                color: '#FEB019',
-              }
-            }
-          },
-        ],
-        tooltip: {
-          fixed: {
-            enabled: true,
-            position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
-            offsetY: 30,
-            offsetX: 60
-          },
-        },
-        legend: {
-          horizontalAlign: 'left',
-          offsetX: 40
-        }
-        };*/
-        
-        
-      
-      
 
-////////////////////////////////////////////////////////////////////////////
-
-//차트 2
-      
-       
-              var options = {
-          series: [{
-          name: 'PRODUCT A',
-          data: [44, 55, 41, 67, 22, 43, 21, 49]
-        }, {
-          name: 'PRODUCT B',
-          data: [13, 23, 20, 8, 13, 27, 33, 12]
-        }, {
-          name: 'PRODUCT C',
-          data: [11, 17, 15, 15, 21, 14, 15, 13]
-        }],
-          chart: {
-          type: 'bar',
-          height: 350,
-          stacked: true,
-          stackType: '100%'
-        },
-        responsive: [{
-          breakpoint: 480,
-          options: {
-            legend: {
-              position: 'bottom',
-              offsetX: -10,
-              offsetY: 0
-            }
-          }
-        }],
-        xaxis: {
-          categories: ['2011 Q1', '2011 Q2', '2011 Q3', '2011 Q4', '2012 Q1', '2012 Q2',
-            '2012 Q3', '2012 Q4'
-          ],
-        },
-        fill: {
-          opacity: 1
-        },
-        legend: {
-          position: 'right',
-          offsetX: 0,
-          offsetY: 50
-        },
-        };
-
-        var chart2 = new ApexCharts(document.querySelector("#chart2"), options);
-        chart2.render();
-
-    
-      
-      
       
 /////////////////
 //차트 3     
@@ -310,8 +142,9 @@ function drawChart(){
 		data: {}, //필요한 데이터
 		success: function(result) {
 			//학년별 학생 수 차트를 그림
-			drawChart1(result);
-			
+			drawChart1(result.studentCntPerGrade);
+			//과목별 학생 수 차트를 그림
+			drawChart2(result.studentCntPerSubject);
 		},
 		error: function() {
 			alert('실패');
@@ -321,9 +154,11 @@ function drawChart(){
 	
 }
 
+//차트 1
 //학년별 학생 수 차트를 그림
 function drawChart1(data){
 	
+
 	//data가 배열로 넘어오니까 빈 배열 만들고 
 	chart_data_arr = [];
 	
@@ -334,7 +169,7 @@ function drawChart1(data){
 		chart_data.x = grade.studentYear;
 		chart_data.y = grade.studentCnt;
 		chart_data_arr.push(chart_data);
-		
+
 	}
 	
 
@@ -370,6 +205,82 @@ function drawChart1(data){
 }
 
 
+//차트 2
+//과목별 학생 수 차트를 그림
+function drawChart2(data){
+	//console.log(data);
+	//console.log(data.과학);
+	
+	
+	//과학.수학
+	let keys = Object.keys(data);
+	//console.log(keys);
+
+    //data가 배열로 넘어오니까 빈 배열 만들고 
+	chart_data_arr = [];
+	
+	//클래스를 만들고 변수를 정해줌
+	//하나씩 반복문으로 돌려서 위에서 만들어준 빈배열에 넣기 
+	for(const key of keys){
+		chart_data = new Object();
+		chart_data.name = key;
+		chart_data.data = data[key];
+		chart_data_arr.push(chart_data);
+		
+	}
+	
+
+	console.log(chart_data_arr);
+	
+	
+	
+	var options = {
+		/*series: [{
+			name: 'PRODUCT A',
+			data: [44, 55, 41]
+		}, {
+			name: 'PRODUCT B',
+			data: [13, 23,32 ]
+		}, {
+			name: 'PRODUCT C',
+			data: [11, 17, 15]
+		}]*/
+		series : chart_data_arr
+		,
+		chart: {
+			type: 'bar',
+			height: 350,
+			stacked: true,
+			stackType: '100%'
+		},
+		responsive: [{
+			breakpoint: 480,
+			options: {
+				legend: {
+					position: 'bottom',
+					offsetX: -10,
+					offsetY: 0
+				}
+			}
+		}],
+		xaxis: {
+			categories: ['1학년','2학년','3학년'
+			],
+		},
+		fill: {
+			opacity: 1
+		},
+		legend: {
+			position: 'right',
+			offsetX: 0,
+			offsetY: 50
+		},
+	};
+
+    var chart2 = new ApexCharts(document.querySelector("#chart2"), options);
+    chart2.render();
+    
+}
 
 
 
