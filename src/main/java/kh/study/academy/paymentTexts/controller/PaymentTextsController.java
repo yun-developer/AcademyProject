49def279f968ac9aa5762c1d3cec934aba55e323
kwseon <javaakws@gmail.com>
2022-11-25 16:24:28 +0900

    정기 문자 보내기 수정 중

--- src/main/java/kh/study/academy/paymentTexts/controller/PaymentTextsController.java
+++ src/main/java/kh/study/academy/paymentTexts/controller/PaymentTextsController.java
@@ -3,6 +3,7 @@ package kh.study.academy.paymentTexts.controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,11 +12,14 @@ import org.springframework.stereotype.Component;
 @EnableScheduling 
 public class PaymentTextsController {
 	
-	@Scheduled(cron = "0 1 18 00 * ?", zone="Asia/Seoul")   //매월 1일 18시 00분에 실행
+	//@Async
+	//@Scheduled(fixedRate = 1000)
+	@Scheduled(cron = "0 12 16 25 * ?", zone="Asia/Seoul")   //매월 1일 18시 00분에 실행
     public void firstTask() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-        Date now = new Date();
-        String strDate = sdf.format(now);
-        System.out.println("현재시간:  " + strDate);
+		 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+		 Date now = new Date(); 
+		 String strDate = sdf.format(now);
+		 System.out.println("현재시간:  " + strDate);
+		 
     }
 }
