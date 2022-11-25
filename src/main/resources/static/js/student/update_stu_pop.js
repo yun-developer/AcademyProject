d6761e652790895f485a4fec4cc8adffdd722bc7
yun-developer <yun.wjddms@gmail.com>
2022-11-25 20:16:18 +0900

    학급 이동 시 이미 수강중인 정보와 중복될때 버튼 비활성화 + 알람 띄우기

---
+++ src/main/resources/static/js/student/update_stu_pop.js
@@ -0,0 +1,37 @@
+//수업 목록 셀렉트 박스
+const selectLesson = document.querySelector('#selectBeforeLesson');
+const updateBtn = document.querySelector('#updateBtn');
+const lessonInfoCodes = document.querySelectorAll('.lessonInfoCodes');
+
+
+
+selectLesson.addEventListener('change', function (){
+	
+	
+	updateBtn.setAttribute("class", "btn btn btn-primary");
+	updateBtn.innerText = "저 장";
+	updateBtn.disabled = false;
+	
+	//셀렉트 박스에서 선택한 값
+	let selectlessonInfoCode = selectLesson.options[selectLesson.selectedIndex].value;
+	
+	for(const code of lessonInfoCodes){
+		
+		if(selectlessonInfoCode == code.value){
+			
+			//태그 속성 추가 변경
+				updateBtn.setAttribute("class", "btn btn-secondary");
+			
+				updateBtn.innerText = "배 정 불 가";
+				updateBtn.disabled = true;
+
+				Swal.fire('이동 불가', '학생이 이미 수강중인 학급입니다.', 'warning');
+				
+			
+			return;
+		}
+		
+	}
+	
+
+});
