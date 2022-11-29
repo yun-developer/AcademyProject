4c8aee140eb042b74c1335feb62d75a37a2a23e9
kwseon <javaakws@gmail.com>
2022-11-29 13:58:42 +0900

    학생 정기 문자 발송 완료

--- src/main/java/kh/study/academy/student/controller/PaymentTextsController.java
+++ src/main/java/kh/study/academy/student/controller/PaymentTextsController.java
@@ -27,6 +27,7 @@ public class PaymentTextsController {
 	
 	//@Async
 	//@Scheduled(fixedRate = 100000)
+	//달 바뀌면 isPay = 'n'으로 바꾸기
 	@Scheduled(cron = "0 00 12 1 * ?", zone="Asia/Seoul")  //매월 1일 12시 00분 00초에 실행 (거꾸로 초분시... 이렇게 보면 됨)
 	public void stuUpdateIsPayNone() {
 		studentService.stuUpdateIsPayNone();
@@ -37,43 +38,40 @@ public class PaymentTextsController {
 	
 	//@Async
 	//@Scheduled(fixedRate = 1000000)
-	@Scheduled(cron = "0 47 13 29 * ?", zone="Asia/Seoul")   //매월 1일 18시 00분에 00초 실행
+	//매달 1일에 학원비 수납 문자 보내기
+	@Scheduled(cron = "0 0 18 1 * ?", zone="Asia/Seoul")   //매월 1일 18시 00분에 00초 실행
 	public void paymentSms() {
-		// paymentCode 새로 만들때 예전 코드 지우기  -> transaction으로 서비스 임플
-		// 달 바뀌면 isPay = 'n'으로 바꾸기
 		
 		String api_key = "NCSOHSUJTJFE1FES";
 	    String api_secret = "ZURBUMXTPGZDDJXBZITGMV95C5MVAID2";
 	    Message coolsms = new Message(api_key, api_secret);
 		
-		
+	    
 		for(StudentVO stu :  studentService.selectStuLessonList()) {
 			
-			if(stu.getStudentStatus() == "Y") {
 				
-				if(stu.getStudentLessonInfoList().get(0).getLessonInfoVO() != null) {
-					stu.getStudentCode();
-					String studentName = stu.getStudentName();
-					String studentTell = stu.getStudentTell();
-					String subjectName = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getSubjectVO().getSubjectName();
-					String stepName = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getStepVO().getStepName();
-					int money = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getMoney();
-					
-					// 4 params(to, from, type, text) are mandatory. must be filled
-					HashMap<String, String> params = new HashMap<String, String>();
-					params.put("to", studentTell);	// 수신전화번호
-					params.put("from", "01099301637");	// 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
-					params.put("type", "SMS");
-					params.put("text", studentName + " 학생의 납부일임을 알려드립니다." + subjectName + "-" + stepName + " 반의 수강료는 "+ money + "원 입니다. \n[PotatoAcademy]");
-					params.put("app_version", "test app 1.2"); // application name and version
-					
-					try {
-						JSONObject obj = (JSONObject) coolsms.send(params);
-						System.out.println(obj.toString());
-					} catch (CoolsmsException e) {
-						System.out.println(e.getMessage());
-						System.out.println(e.getCode());
-					}
+			if(stu.getStudentLessonInfoList().get(0).getLessonInfoVO() != null) {
+				stu.getStudentCode();
+				String studentName = stu.getStudentName();
+				String studentTell = stu.getStudentTell();
+				String subjectName = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getSubjectVO().getSubjectName();
+				String stepName = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getStepVO().getStepName();
+				int money = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getMoney();
+				
+				// 4 params(to, from, type, text) are mandatory. must be filled
+				HashMap<String, String> params = new HashMap<String, String>();
+				params.put("to", studentTell);	// 수신전화번호
+				params.put("from", "01099301637");	// 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+				params.put("type", "SMS");
+				params.put("text", studentName + " 학생의 납부일임을 알려드립니다. " + subjectName + "-" + stepName + "반의 수강료는 "+ money + "원 입니다.\n[PotatoAcademy]");
+				params.put("app_version", "test app 1.2"); // application name and version
+				
+				try {
+					JSONObject obj = (JSONObject) coolsms.send(params);
+					System.out.println(obj.toString());
+				} catch (CoolsmsException e) {
+					System.out.println(e.getMessage());
+					System.out.println(e.getCode());
 				}
 			}
 			}
