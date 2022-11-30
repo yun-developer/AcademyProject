06f9ee4c64d8635a0ab4f5823b050d363224f3e8
sowon22 <thdnjs2626@naver.com>
2022-11-30 12:17:23 +0900

    교실 목록 삭제시 안내창 뜨게 수정완료!

--- src/main/resources/static/js/admin/lesson_room.js
+++ src/main/resources/static/js/admin/lesson_room.js
@@ -95,21 +95,85 @@ function lessonRoomDeleteAjax(){
 	//체크한 cartCode 다 들고 온다. cartCode는 체크박스안에 들어있다. 전체 체크박스에서 체크된 것을 들고 오면 됨
 	const checkedChks = document.querySelectorAll('.chk:checked'); // -> 내가 선택한 체크박스들(여러개) // 나는 클래스가 .chk인 애를 선택할 꺼야 클래스호출은 .을 써주기
 	
+	
+	//체크한 체크박스가 없으면 뜨는 알림창
 	if(checkedChks.length == 0){
-		 const modal = new bootstrap.Modal('#deleteModal');  // #myModal -> 모달창의 id를 가지고 오는 것이다.
-	     modal.show();
+		/* const modal = new bootstrap.Modal('#deleteModal');  // #myModal -> 모달창의 id를 가지고 오는 것이다.
+	     modal.show();*/
+	     Swal.fire({
+		  title: '삭제할 교실을 선택하세요!',
+		  icon: 'warning',
+		  confirmButtonText: '확인'
+		})
 		return ;
 	}
-	// 문자열로 만들어서 던져 줌
-	let lessonRoomCodes = '';
-	for(const checkedChk of checkedChks){
-		lessonRoomCodes = lessonRoomCodes + checkedChk.value + ',';
-	}	
+	
+	// 
+	else{
+		Swal.fire({
+		   title: '정말 삭제하시겠습니까?',
+		   icon: 'warning',
+		   
+		   showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+		   confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+		   cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+		   confirmButtonText: '승인', // confirm 버튼 텍스트 지정
+		   cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+		   
+		   
+		}).then(result => {
+		   // 만약 Promise리턴을 받으면,
+		   if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
+			
+			//삭제 완료 alert 시작
+			Swal.fire({
+				title: '교실 삭제 완료',
+				text: '해당 교실이 삭제되었습니다.',
+				icon: 'success',
 
-	LessonRoomForm.querySelector('input').value = lessonRoomCodes;  // cartCode셋팅
+				showCancelButton: false, // cancel버튼 보이기. 기본은 원래 없음
+				confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+				cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+				confirmButtonText: '확인', // confirm 버튼 텍스트 지정
+				cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+
+				reverseButtons: true, // 버튼 순서 거꾸로
+
+			}).then(result => {
+				// 만약 Promise리턴을 받으면,
+				if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
+
+				//문자열로 만들어서 던져 줌
+				let lessonRoomCodes = '';
+				for(const checkedChk of checkedChks){
+					lessonRoomCodes = lessonRoomCodes + checkedChk.value + ',';
+				}	
+				LessonRoomForm.querySelector('input').value = lessonRoomCodes;  // cartCode셋팅
+				
+				
+				LessonRoomForm.submit();
+				
+
+				}
+			});
+			//삭제 완료 alert 끝
+			
+			
+	
+			
+
+			}
+		});
+	
+}
+
 	
 	
-	LessonRoomForm.submit();
+	
+	
+	
+	
+	
 	
 }
 //const modal = new bootstrap.Modal('#bb');
