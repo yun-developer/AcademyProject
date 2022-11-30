b5f53a4369784e37dc6816a89350b906a7d3aa3a
yun-developer <yun.wjddms@gmail.com>
2022-11-30 13:47:53 +0900

    학생 편성시 해당 주의 출결데이터에 필요한 수업 날짜 구하는 메소드 추가

--- src/main/java/kh/study/academy/config/DateUtil.java
+++ src/main/java/kh/study/academy/config/DateUtil.java
@@ -3,6 +3,7 @@ package kh.study.academy.config;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 
 public class DateUtil {
 	
@@ -17,7 +18,7 @@ public class DateUtil {
 		return startTime;
 	}
 	
-	//요일에 해당하는 날짜 가져오기
+	//요일에 해당하는 날짜 가져와서 FullCalendar에 맞춰 변형
 	public static String[] getLessonDatebyDay(String dayCode, String lessonTime) {
 		
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
@@ -44,6 +45,7 @@ public class DateUtil {
 		
 		/** 오늘날짜에 해당하는 월, 주,월요일 날짜를 가져오기 **/
 		Calendar startDate = Calendar.getInstance();
+		
 		//startDate.isSet(Calendar.MONTH); /** 오늘 날짜의 월 셋팅**/
 		//startDate.isSet(Calendar.WEEK_OF_MONTH); /** 오늘 날짜의 주 셋팅 **/
 		startDate.set(Calendar.DAY_OF_WEEK, 2); /** 월요일 셋팅 **/
@@ -62,8 +64,8 @@ public class DateUtil {
 		String edate ="";
 		edate = df.format(endDate.getTime());
 		
-		System.out.println("해당 주의 월요일:"+ sdate);
-		System.out.println("해당주의 금요일:"+edate);
+		//System.out.println("해당 주의 월요일:"+ sdate);
+		//System.out.println("해당주의 금요일:"+edate);
 		
 		//하루 더하기
 		//startDate.add(Calendar.DAY_OF_WEEK, 1);
@@ -98,9 +100,56 @@ public class DateUtil {
 	}
 	
 	
-	
-	
-	
+	//수업요일에 해당하는 날짜를 해당 월에 있는 만큼 가져와서 출결날짜로 가져가기
+	public static String getAttendDate(String dayCode) {
+		
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+		
+		//1-일, 2-월, 3-화, 4-수, 5-목, 6-금, 7-토
+		int dayNum = 0;
+		for(int i =2; i<=7; i++) {
+			if(dayCode.equals("Mon")) {
+				dayNum = 2;
+			}
+			else if (dayCode.equals("Tue")) {
+				dayNum = 3;
+			}
+			else if (dayCode.equals("Wed")) {
+				dayNum = 4;
+			}
+			else if (dayCode.equals("Thu")) {
+				dayNum = 5;
+			}
+			else if (dayCode.equals("Fri")) {
+				dayNum = 6;
+			}
+		}
+		
+		Calendar lessonDate = Calendar.getInstance();
+		lessonDate.set(Calendar.DAY_OF_WEEK, dayNum);
+		String lessonDateStr ="";
+		lessonDateStr = df.format(lessonDate.getTime());
+		
+//		Calendar firstDate = Calendar.getInstance();
+//		Calendar finalDate = Calendar.getInstance();
+//		
+//		//첫번째 날짜
+//		firstDate.set(Calendar.DAY_OF_WEEK, 1);
+//		//마지막 날짜
+//		finalDate.set(Calendar.DAY_OF_WEEK, 7);
+//		
+//		//시작날짜와 끝 날짜를 비교해, 시작날짜가 작거나 같은 경우 출력
+//		while( firstDate.compareTo(finalDate) !=1 ){
+//			//출력
+//			System.out.println(firstDate.compareTo(finalDate));
+//			System.out.println("!"+firstDate.getTime());
+//			System.out.println("!!"+finalDate.getTime());
+//			//시작날짜 + 1 일
+//			firstDate.add(Calendar.DATE, 1);
+//		}
+		
+		return lessonDateStr;
+	}
 	
 	
 	////////////////////
