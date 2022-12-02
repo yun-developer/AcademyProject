499d6246bfff1ddb9f4f5e8be64f07c258e85228
yun-developer <yun.wjddms@gmail.com>
2022-12-02 15:32:32 +0900

    코드 정리

--- src/main/resources/static/js/teacher/join_page.js
+++ src/main/resources/static/js/teacher/join_page.js
@@ -34,191 +34,191 @@ const isTellDoubleChkTag = document.querySelector('#isTellDoubleChk')
 
 //아이디 중복 검사 여부 확인 후 회원가입 진행
 function isDoubleChk (){
-	
-	//회원가입 진행 폼태그
-	const joinForm = document.querySelector('#joinForm');
-	
-	//id 중복검사를 하지 않았다면
-	if(isDoubleChkTag.value=="unChk"){
-		
-		 Swal.fire({
+   
+   //회원가입 진행 폼태그
+   const joinForm = document.querySelector('#joinForm');
+   
+   //id 중복검사를 하지 않았다면
+   if(isDoubleChkTag.value=="unChk"){
+      
+       Swal.fire({
                     icon: 'warning',
                     title: '가입 불가',
                     text: 'ID 중복 검사를 실행해주세요!',
                 });
-		return;
-	}
-	
-	//전화번호 중복검사를 하지 않았다면
-	if(isTellDoubleChkTag.value=="unChk"){
-		
-		 Swal.fire({
+      return;
+   }
+   
+   //전화번호 중복검사를 하지 않았다면
+   if(isTellDoubleChkTag.value=="unChk"){
+      
+       Swal.fire({
                     icon: 'warning',
                     title: '가입 불가',
                     text: '전화번호 중복 검사를 실행해주세요!',
                 });
-		return;
-	}
-	
+      return;
+   }
+   
 
-/*	//ajax start
-	$.ajax({
-		url: '/test/regScoreAjax', //요청경로
-		type: 'post',
-		data: {'studentCode':stuentCodeValue,'subjectCode':selectSubValue,'testDate':selectDateValue}, //필요한 데이터
-		success: function(result) {
-			
-				
-				 Swal.fire({
-		 		  title: '평가 등록 실패',
-				   text: '해당 학생의 평가 점수가 이미 존재합니다',
-				   icon: 'error',
-				   
-				   showCancelButton: false, // cancel버튼 보이기. 기본은 원래 없음
-				   confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
-				   cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
-				   confirmButtonText: '확인', // confirm 버튼 텍스트 지정
-				   
-				   
-				}).then(result => {
-				   // 만약 Promise리턴을 받으면,
-				   if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
-				   
-				   //d
-				   
-				   }
-				});
-			
-		},
-		error: function() {
-			alert('실패');
-		}
-	});
-	//ajax end
-	*/
-	joinForm.submit();
+/*   //ajax start
+   $.ajax({
+      url: '/test/regScoreAjax', //요청경로
+      type: 'post',
+      data: {'studentCode':stuentCodeValue,'subjectCode':selectSubValue,'testDate':selectDateValue}, //필요한 데이터
+      success: function(result) {
+         
+            
+             Swal.fire({
+               title: '평가 등록 실패',
+               text: '해당 학생의 평가 점수가 이미 존재합니다',
+               icon: 'error',
+               
+               showCancelButton: false, // cancel버튼 보이기. 기본은 원래 없음
+               confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+               cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+               confirmButtonText: '확인', // confirm 버튼 텍스트 지정
+               
+               
+            }).then(result => {
+               // 만약 Promise리턴을 받으면,
+               if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
+               
+               //d
+               
+               }
+            });
+         
+      },
+      error: function() {
+         alert('실패');
+      }
+   });
+   //ajax end
+   */
+   joinForm.submit();
 }
 
 
 
 //아이디 중복 검사 결과
 function idDoubleCheck (){
-	
-	const inputTeacherIdValue = inputTeacherId.value;
-	 
-	if(inputTeacherIdValue==''){
-		// 요소의 placeholder 속성에 특정값을 설정한다.
-    	inputTeacherId.setAttribute("placeholder", "*ID를 입력하세요.");
-		// 커서를 아이디 인풋태그로 이동한다.
-	    inputTeacherId.focus();
-	    return;
-	}	
-	
-	//ajax start
-	$.ajax({
-	   url: '/teacher/idDoubleCheckAjax', //요청경로
-	    type: 'post',
-	    data:{'teacherId':inputTeacherIdValue}, //필요한 데이터
-	    success: function(result) {
-		 
-			//중복아이디가 있을 경우
-			if(result.teacherId != null){
-				Swal.fire({
+   
+   const inputTeacherIdValue = inputTeacherId.value;
+    
+   if(inputTeacherIdValue==''){
+      // 요소의 placeholder 속성에 특정값을 설정한다.
+       inputTeacherId.setAttribute("placeholder", "*ID를 입력하세요.");
+      // 커서를 아이디 인풋태그로 이동한다.
+       inputTeacherId.focus();
+       return;
+   }   
+   
+   //ajax start
+   $.ajax({
+      url: '/teacher/idDoubleCheckAjax', //요청경로
+       type: 'post',
+       data:{'teacherId':inputTeacherIdValue}, //필요한 데이터
+       success: function(result) {
+       
+         //중복아이디가 있을 경우
+         if(result.teacherId != null){
+            Swal.fire({
                     icon: 'error',
                     title: '가입 불가',
                     text: '사용 불가능한 아이디입니다',
                 });
-                	inputTeacherId.value='';
-					inputTeacherId.focus();
-			}
-			//중복 아이디가 없을 경우
-			else{
-			 	 Swal.fire({
+                   inputTeacherId.value='';
+               inputTeacherId.focus();
+         }
+         //중복 아이디가 없을 경우
+         else{
+              Swal.fire({
                     icon: 'success',
                     title: '가입 가능',
                     text: '사용 가능한 아이디입니다',
                 });
-			}
-	    },
-	    error: function(){
-	       alert('실패');
-	    }
-	});
-	//ajax end
-		
-	//중복검사 수행 확인
-	isDoubleChkTag.setAttribute("value", "Chk");
+         }
+       },
+       error: function(){
+          alert('실패');
+       }
+   });
+   //ajax end
+      
+   //중복검사 수행 확인
+   isDoubleChkTag.setAttribute("value", "Chk");
 }
 
 
 //전화번호 중복 검사 결과 
 function isTellDoubleCheck(){
-	
-	const inputTeacherTellValue = inputTeacherTell.value;
-	
-	if (inputTeacherTellValue ==''){
-		// 요소의 placeholder 속성에 특정값을 설정한다.
-    	inputTeacherTell.setAttribute("placeholder", "*전화번호를 입력하세요.");
-		// 커서를 전화번호 인풋태그로 이동한다.
-	    inputTeacherTell.focus();
-	    return;
-	}
-	
-	//ajax start
-	$.ajax({
-	   url: '/teacher/tellDoubleCheckAjax', //요청경로
-	    type: 'post',
-	    data:{'teacherTell':inputTeacherTellValue}, //필요한 데이터
-	    success: function(result) {
-		 
-			//중복아이디가 있을 경우
-			if(result.teacherTell != null){
-				Swal.fire({
+   
+   const inputTeacherTellValue = inputTeacherTell.value;
+   
+   if (inputTeacherTellValue ==''){
+      // 요소의 placeholder 속성에 특정값을 설정한다.
+       inputTeacherTell.setAttribute("placeholder", "*전화번호를 입력하세요.");
+      // 커서를 전화번호 인풋태그로 이동한다.
+       inputTeacherTell.focus();
+       return;
+   }
+   
+   //ajax start
+   $.ajax({
+      url: '/teacher/tellDoubleCheckAjax', //요청경로
+       type: 'post',
+       data:{'teacherTell':inputTeacherTellValue}, //필요한 데이터
+       success: function(result) {
+       
+         //중복아이디가 있을 경우
+         if(result.teacherTell != null){
+            Swal.fire({
                     icon: 'error',
                     title: '가입 불가',
                     text: '이 전화번호로 가입된 회원이 있습니다',
                 });
-                	inputTeacherTell.value='';
-					inputTeacherTell.focus();
+                   inputTeacherTell.value='';
+               inputTeacherTell.focus();
                 
                 
-			}
-			//중복 아이디가 없을 경우
-			else{
-			
-				  Swal.fire({
+         }
+         //중복 아이디가 없을 경우
+         else{
+         
+              Swal.fire({
                     icon: 'success',
                     title: '가입 가능',
                     text: '이 전화번호로 가입된 회원이 없습니다',
                 });
-			}
-	    },
-	    error: function(){
-	       alert('실패');
-	    }
-	});
-	//ajax end
-	
-	//중복검사 수행 확인
-	isTellDoubleChkTag.setAttribute("value", "Chk");
-	
+         }
+       },
+       error: function(){
+          alert('실패');
+       }
+   });
+   //ajax end
+   
+   //중복검사 수행 확인
+   isTellDoubleChkTag.setAttribute("value", "Chk");
+   
 }
 
 
 
 
 function resetInput(event){
-	inputTeacherId.value='';
-	inputTeacherId.focus();
+   inputTeacherId.value='';
+   inputTeacherId.focus();
 }
 
 function isDoubleChkChange(event){
 
-	isDoubleChkTag.setAttribute("value", "unChk");
+   isDoubleChkTag.setAttribute("value", "unChk");
 }
 function tellDoubleChkChange(event){
 
-	isTellDoubleChkTag.setAttribute("value", "unChk");
+   isTellDoubleChkTag.setAttribute("value", "unChk");
 }
 
 
@@ -246,30 +246,30 @@ inputTeacherTell.addEventListener('keydown', tellDoubleChkChange)
 
 //이메일 입력시
 endEmail.addEventListener('change', function getTotalEmail() {
-	
-	const emailSelectDiv = document.querySelector('#emailSelect');
-	
-	totalEmail.value = inputTeacherEmail.value + middleEmail.innerText + endEmail.value;
-	
-	//직접 선택을 선택하면
-	if(endEmail.value == 'selfEmail'){
-		
-		emailSelectDiv.innerHTML='';
-		
-		let str = '';
-		str = '<input type="text" class="form-control" id="newEndEmail" value="" >';
-		
-		emailSelectDiv.insertAdjacentHTML('afterbegin', str);
-	}
-	
+   
+   const emailSelectDiv = document.querySelector('#emailSelect');
+   
+   totalEmail.value = inputTeacherEmail.value + middleEmail.innerText + endEmail.value;
+   
+   //직접 선택을 선택하면
+   if(endEmail.value == 'selfEmail'){
+      
+      emailSelectDiv.innerHTML='';
+      
+      let str = '';
+      str = '<input type="text" class="form-control" id="newEndEmail" value="" >';
+      
+      emailSelectDiv.insertAdjacentHTML('afterbegin', str);
+   }
+   
 });
 
 $(document).on("click", "#newEndEmail", function() {
-	
-	$(document).on("keyup", "#newEndEmail", function() {
-	
-		const newEndEmaildiv = document.querySelector('#newEndEmail');
-		totalEmail.value = inputTeacherEmail.value + middleEmail.innerText + newEndEmaildiv.value;
-	});
-	
+   
+   $(document).on("keyup", "#newEndEmail", function() {
+   
+      const newEndEmaildiv = document.querySelector('#newEndEmail');
+      totalEmail.value = inputTeacherEmail.value + middleEmail.innerText + newEndEmaildiv.value;
+   });
+   
 });
