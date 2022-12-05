f3a0eaf5b3ce2f835ed41f1c49c5f8a83d716b41
yun-developer <yun.wjddms@gmail.com>
2022-12-05 17:17:16 +0900

    불필요한 코드 정리

--- src/main/java/kh/study/academy/student/controller/PaymentTextsController.java
+++ src/main/java/kh/study/academy/student/controller/PaymentTextsController.java
@@ -25,8 +25,7 @@ public class PaymentTextsController {
 	private StudentService studentService;
 	
 	
-	//@Async
-	//@Scheduled(fixedRate = 100000)
+	
 	//달 바뀌면 isPay = 'n'으로 바꾸기
 	@Scheduled(cron = "0 00 12 1 * ?", zone="Asia/Seoul")  //매월 1일 12시 00분 00초에 실행 (거꾸로 초분시... 이렇게 보면 됨)
 	public void stuUpdateIsPayNone() {
@@ -36,8 +35,6 @@ public class PaymentTextsController {
 	
 	
 	
-	//@Async
-	//@Scheduled(fixedRate = 1000000)
 	//매달 1일에 학원비 수납 문자 보내기
 	@Scheduled(cron = "0 0 18 1 * ?", zone="Asia/Seoul")   //매월 1일 18시 00분에 00초 실행
 	public void paymentSms() {
@@ -57,15 +54,12 @@ public class PaymentTextsController {
 				String stepName = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getStepVO().getStepName();
 				int money = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getMoney();
 				
-				//String isPay = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getPaymentVO().getIsPay();
 				
-				// 4 params(to, from, type, text) are mandatory. must be filled
 				HashMap<String, String> params = new HashMap<String, String>();
 				params.put("to", studentTell);	// 수신전화번호
 				params.put("from", "01099301637");	// 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
 				params.put("type", "SMS");
 				params.put("text", studentName + " 학생의 납부일임을 알려드립니다. " + subjectName + "-" + stepName + "반의 수강료는 "+ money + "원 입니다.\n[PotatoAcademy]");
-//				params.put("text", isPay);
 				params.put("app_version", "test app 1.2"); // application name and version
 				
 				try {
@@ -98,7 +92,6 @@ public class PaymentTextsController {
 				String stepName = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getStepVO().getStepName();
 				int money = stu.getStudentLessonInfoList().get(0).getLessonInfoVO().getMoney();
 				
-				// 4 params(to, from, type, text) are mandatory. must be filled
 				HashMap<String, String> params = new HashMap<String, String>();
 				params.put("to", studentTell);	// 수신전화번호
 				params.put("from", "01099301637");	// 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
