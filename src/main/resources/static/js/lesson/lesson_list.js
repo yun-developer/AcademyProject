356ff8f401514264ad5747e300d29975aecaf160
yun-developer <yun.wjddms@gmail.com>
2022-12-07 20:56:16 +0900

    side 영역 css 수정

--- src/main/resources/static/js/lesson/lesson_list.js
+++ src/main/resources/static/js/lesson/lesson_list.js
@@ -140,6 +140,8 @@ function drawCalendar(result){
 		
 		
 		var options = {
+				height: 500,
+				
                 // Tool Bar 목록 document : https://fullcalendar.io/docs/toolbar
                 headerToolbar: {
                     left: 'prevYear,prev,next,nextYear today',
@@ -185,67 +187,7 @@ function drawCalendar(result){
 
 
 
-    	/*document.addEventListener('DOMContentLoaded', function() {
-        	var calendarEl = document.getElementById('calendar');
-        	
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                // Tool Bar 목록 document : https://fullcalendar.io/docs/toolbar
-                headerToolbar: {
-                    left: 'prevYear,prev,next,nextYear today',
-                    center: 'title',
-                    right: 'dayGridMonth,dayGridWeek'
-                },
-
-                selectable: true,
-                selectMirror: true,
-
-                navLinks: true, // can click day/week names to navigate views
-                editable: true,
-                
-
-             
-                dayMaxEvents: false, // allow "more" link when too many events
-                // 이벤트 객체 필드 document : https://fullcalendar.io/docs/event-object
-                events: [
-                    {
-                    title: 'All Day Event',
-                    start: '2022-11-21'
-                    },
-                    {
-                    title: 'Long Event',
-                    start: '2022-07-07',
-                    end: '2022-07-10'
-                    },
-                    {
-                    groupId: 999,
-                    title: 'Repeating Event',
-                    start: '2022-11-24T16:00:00'
-                    },
-                    {
-                    groupId: 999,
-                    title: 'Repeating Event',
-                    start: '2022-07-16T16:00:00'
-                    },
-              
-                    {
-                    title: 'Meeting',
-                    start: '2022-07-12T10:30:00',
-                    end: '2022-07-12T12:30:00'
-                    },
-                    {
-                    title: 'Lunch',
-                    start: '2022-07-12T12:00:00'
-                    },
-                    {
-                    title: 'Click for Google',
-                    url: 'http://google.com/',
-                    start: '2022-07-28'
-                    }
-                ]
-            });
-
-            calendar.render();
-        });*/
+    	
 
 
 
