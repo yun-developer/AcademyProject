3e191395c2ef19f8d838b303e36c36c039b823d2
kwseon <javaakws@gmail.com>
2022-12-08 10:51:19 +0900

    학생 배정 페이지 페이징처리

--- src/main/resources/static/js/student/assign_page.js
+++ src/main/resources/static/js/student/assign_page.js
@@ -122,7 +122,13 @@ $('.updatePopup').on("click",function(e){
 
 
 
-
+//페이징 라이브러리
+var box = paginator({
+    table: document.getElementById("table_box_bootstrap").getElementsByTagName("table")[0],
+    box_mode: "list",
+});
+box.className = "box";
+document.getElementById("table_box_bootstrap").appendChild(box);
 
 
 
