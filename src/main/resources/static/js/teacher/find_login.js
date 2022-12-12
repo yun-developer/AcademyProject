ae8c5ccc8b6b9466387dfeb0c4c504ed7ad55b0a
bb9oo <hyeonjubb@gmail.com>
2022-12-12 09:27:54 +0900

    아이디 비번 찾기 인풋태그가 빈값일때는 쿼리 실행하지 않게 변경

--- src/main/resources/static/js/teacher/find_login.js
+++ src/main/resources/static/js/teacher/find_login.js
@@ -28,8 +28,7 @@ function findLoginIdAjax(){
 	//teacherTell 값
 	const teacherTell = document.querySelector('#findTell').value;
 		
-	console.log(teacherName);
-	console.log(teacherTell);
+	
 	
 	let str = '';
 	
@@ -41,33 +40,37 @@ function findLoginIdAjax(){
 		data: {'teacherName':teacherName, 'teacherTell':teacherTell}, //필요한 데이터
 		success: function(result) {
 			
-			
-		
-			
-			if(result.check == 0){
+			//아이디 찾기 input 태그가 빈값이 아니라면 아이디 찾기 조회결과 나오기
+			if(teacherName!='' & teacherTell!=''){
+				if(result.check == 0){
 				str += ' <div class="col-md-4" style="float: none; margin: 0 auto;">  ';
 				str += ' <div>  ';
 				str += ' 아이디 조회 결과 입력하신 정보와 일치하는 아이디가 존재하지 않습니다.  ';
 				str += ' </div>  ';
 				str += ' </div>  ';
+				}
+				else if(result.check == 1){
+					str += ' <div class="col-md-4" style="float: none; margin: 0 auto;">  ';
+					str += ' <div class="mb-3">  ';
+					str += ' 아이디 조회 결과 입력하신 정보와 일치하는 아이디는 아래와 같습니다 ';
+					str += ' </div> ';
+					str += ` <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+							<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+							</svg> ${result.teacherId}</div>` ;
+					str += ' </div> ';
+				}
+				
+				
+				const findResult = document.querySelector('#findResult');
+	
+				findResult.innerHTML = ''; /*내용을 지우는것*/
+	
+				findResult.insertAdjacentHTML('beforeend', str);
+				
 			}
-			else if(result.check == 1){
-				str += ' <div class="col-md-4" style="float: none; margin: 0 auto;">  ';
-				str += ' <div class="mb-3">  ';
-				str += ' 아이디 조회 결과 입력하신 정보와 일치하는 아이디는 아래와 같습니다 ';
-				str += ' </div> ';
-				str += ` <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-						<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-						</svg> ${result.teacherId}</div>` ;
-				str += ' </div> ';
-			}
+		
 			
 			
-			const findResult = document.querySelector('#findResult');
-
-			findResult.innerHTML = ''; /*내용을 지우는것*/
-
-			findResult.insertAdjacentHTML('beforeend', str);
 			
 		},
 		error: function() {
@@ -96,6 +99,10 @@ $(document).on("click", "#sendPhoneNumber", function() {
 
 	let str = '';
 	let teacherTell = $('#teacherTell').val();
+	
+	console.log(teacherName);
+	console.log(teacherId);
+	console.log(teacherTell);
 
 
 	$.ajax({
@@ -106,7 +113,11 @@ $(document).on("click", "#sendPhoneNumber", function() {
 		},
 		success: function(res) {
 			
-			//입력한 정보(이름 아이디 전화번호)에 맞는 회원이 있다면 인증번호 보내고 인증번호 입력창 생성 
+			
+			//비밀번호 찾기 input 태그가 모두 빈값이 아니라면 비밀번호 찾기 조회결과 나오기
+			if(teacherName!='' & teacherId!='' & teacherTell!=''){
+				alert(1111)
+							//입력한 정보(이름 아이디 전화번호)에 맞는 회원이 있다면 인증번호 보내고 인증번호 입력창 생성 
 			if (res != "") {
 
 				//Swal.fire('인증번호 발송 완료!')
@@ -216,6 +227,13 @@ $(document).on("click", "#sendPhoneNumber", function() {
 					})
 				}
 			})
+			
+			
+			}
+			
+			
+			
+
 		}
 	})
 });
